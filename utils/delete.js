1fde023555575cba592a0ac0ba607a23d58a8c5e
mendy hershkop <hers.mm@gmail.com>
2025-12-03 12:48:26 +0200

    create del function

---
+++ utils/delete.js
@@ -0,0 +1,20 @@
+import { db } from "../db/database.js";
+export default function del(db, id){
+    let results= null
+    for(let i=0; i<db.length; i++) {
+        if (db[i].id == id){
+            results = db[i]
+            db.splice(i,1)
+        }
+         
+    }
+        if (results == null) {
+            throw new Error("ID not fuond");
+            
+        }
+    return db
+}
+// console.log(db);
+// del(db,"123")
+// console.log(db);
+
